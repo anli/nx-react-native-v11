ef45848d46e7a083395ebadfb54edb4548d7f0f2
Su Anli <su.anli@anlitz.com>
2023-11-19 15:42:38 +0800

    chore(audio-app): add nativewind

--- audio-app/src/app/app.tsx
+++ audio-app/src/app/app.tsx
@@ -6,7 +6,7 @@ import { Text, View } from 'react-native';
 
 const HomeScreen = () => {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View className="flex-1 items-center justify-center">
       <Text>Home Screen</Text>
     </View>
   );
