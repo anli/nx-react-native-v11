9dc9e35e1e93d2d444419ca8119a9fd1b6c418c6
Su Anli <su.anli@anlitz.com>
2023-12-07 11:37:45 +0800

    chore: remove airbnb-app

--- airbnb-app/src/app/app.spec.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react-native';
-
-import App from './app';
-
-test('renders correctly', () => {
-  const { getByTestId } = render(<App />);
-  expect(getByTestId('heading')).toHaveTextContent('Welcome');
-});
