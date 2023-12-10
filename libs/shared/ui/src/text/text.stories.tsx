75a922eb3b02588bad4dc1e909624b7d34b72a65
Su Anli <anli@users.noreply.github.com>
2023-12-10 12:26:31 +0800

    feat(shared-ui): add text story for all types (#5)

--- libs/shared/ui/src/text/text.stories.tsx
+++ libs/shared/ui/src/text/text.stories.tsx
@@ -1,17 +1,88 @@
-import type { Meta, StoryObj } from '@storybook/react-native';
+import type { Meta, StoryObj } from '@storybook/react';
 import { Text } from './text';
 
 const meta: Meta<typeof Text> = {
-  component: Text,
-  title: 'Text',
+  tags: ['autodocs'],
+  component: (args) => <Text {...args}>Text</Text>,
+  title: 'Design System/Text',
 };
 export default meta;
 type Story = StoryObj<typeof Text>;
 
-export const Default: Story = {
-  render: () => <Text>Text</Text>,
+export const Heading1: Story = {
+  args: {
+    type: 'heading1',
+  },
 };
 
-export const DisplayMedium: Story = {
-  render: () => <Text type="display-medium">Text</Text>,
+export const Heading2: Story = {
+  args: {
+    type: 'heading2',
+  },
+};
+
+export const Heading3: Story = {
+  args: {
+    type: 'heading3',
+  },
+};
+
+export const Heading4: Story = {
+  args: {
+    type: 'heading4',
+  },
+};
+
+export const Title1: Story = {
+  args: {
+    type: 'title1',
+  },
+};
+
+export const Title2: Story = {
+  args: {
+    type: 'title2',
+  },
+};
+
+export const Title3: Story = {
+  args: {
+    type: 'title3',
+  },
+};
+
+export const Title4: Story = {
+  args: {
+    type: 'title4',
+  },
+};
+
+export const Title5: Story = {
+  args: {
+    type: 'title5',
+  },
+};
+
+export const Body1: Story = {
+  args: {
+    type: 'body1',
+  },
+};
+
+export const Body2: Story = {
+  args: {
+    type: 'body2',
+  },
+};
+
+export const Body3: Story = {
+  args: {
+    type: 'body3',
+  },
+};
+
+export const Body4: Story = {
+  args: {
+    type: 'body4',
+  },
 };
