77e2cf4cd245ae5521dcf1308043bce759ffbd1b
Su Anli <anli@users.noreply.github.com>
2023-12-11 15:55:55 +0800

    feat(shared-ui): add renderIcon (#11)

--- libs/shared/ui/src/button/box-button/box-button.tsx
+++ libs/shared/ui/src/button/box-button/box-button.tsx
@@ -4,6 +4,7 @@ import clsx, { ClassValue } from 'clsx';
 import { ColorValue, PressableProps } from 'react-native';
 import { Pressable } from '../../pressable';
 import { ActivityIndicator } from '../../activity-indicator';
+import type { NumberProp, SvgProps } from 'react-native-svg';
 
 type TypeConfigKey =
   | 'solid-primary'
@@ -16,7 +17,7 @@ type TypeConfigs = Record<
   {
     container: ClassValue;
     text: ClassValue;
-    activityIndicatorColor: ColorValue;
+    iconColor: ColorValue;
   }
 >;
 
@@ -24,22 +25,22 @@ const enabledConfigs: TypeConfigs = {
   'solid-primary': {
     container: 'bg-green-500',
     text: 'text-white',
-    activityIndicatorColor: 'white',
+    iconColor: 'white',
   },
   'solid-danger': {
     container: 'bg-red-500',
     text: 'text-white',
-    activityIndicatorColor: 'white',
+    iconColor: 'white',
   },
   'outline-primary': {
     container: 'border-solid border-2 border-green-500',
     text: 'text-green-500',
-    activityIndicatorColor: 'gray',
+    iconColor: 'gray',
   },
   'outline-secondary': {
     container: 'border-solid border-2 border-gray-300',
     text: 'text-black',
-    activityIndicatorColor: 'gray',
+    iconColor: 'gray',
   },
 };
 
@@ -47,22 +48,22 @@ const disabledConfigs: TypeConfigs = {
   'solid-primary': {
     container: 'bg-gray-300',
     text: 'text-white',
-    activityIndicatorColor: 'white',
+    iconColor: 'white',
   },
   'solid-danger': {
     container: 'bg-gray-300',
     text: 'text-white',
-    activityIndicatorColor: 'white',
+    iconColor: 'white',
   },
   'outline-primary': {
     container: 'border-solid border-2 bg-white border-gray-300',
     text: 'text-gray-300',
-    activityIndicatorColor: 'gray',
+    iconColor: 'gray',
   },
   'outline-secondary': {
     container: 'border-solid border-2 bg-white border-gray-300',
     text: 'text-gray-300',
-    activityIndicatorColor: 'gray',
+    iconColor: 'gray',
   },
 };
 
@@ -73,6 +74,7 @@ type SizeConfigs = Record<
   {
     container?: ClassValue;
     textType: TextProps['type'];
+    iconSize: number;
   }
 >;
 
@@ -80,22 +82,27 @@ const sizeConfigs: SizeConfigs = {
   xs: {
     container: 'p-1',
     textType: 'title5',
+    iconSize: 14,
   },
   sm: {
     container: 'p-2',
     textType: 'title4',
+    iconSize: 16,
   },
   md: {
     container: 'p-3',
     textType: 'title3',
+    iconSize: 16,
   },
   lg: {
     container: 'p-4',
     textType: 'title2',
+    iconSize: 18,
   },
   xl: {
     container: 'p-5',
     textType: 'title2',
+    iconSize: 20,
   },
 };
 
@@ -104,6 +111,7 @@ type BoxButtonProps = PressableProps &
     type?: TypeConfigKey;
     size?: SizeConfigKey;
     loading?: boolean;
+    renderIcon?: (props: SvgProps & { size?: NumberProp }) => JSX.Element;
   };
 
 export const BoxButton: FC<BoxButtonProps> = ({
@@ -113,32 +121,42 @@ export const BoxButton: FC<BoxButtonProps> = ({
   disabled = false,
   size = 'md',
   loading = false,
+  renderIcon,
   ...rest
 }) => {
   const typeConfig = disabled ? disabledConfigs[type] : enabledConfigs[type];
   const sizeConfig = sizeConfigs[size];
-  const showText = !loading;
+  const showContent = !loading;
+  const Icon = renderIcon?.({
+    size: sizeConfig.iconSize,
+    fill: typeConfig.iconColor,
+  });
+  const isHorizontal = !!Icon && !!children;
 
   return (
     <Pressable
       className={clsx(
-        'rounded-md active:opacity-50',
+        'rounded-md active:opacity-50  justify-center items-center',
+        isHorizontal ? 'flex-row' : '',
         typeConfig.container,
         sizeConfig?.container
       )}
       disabled={disabled}
       {...rest}
     >
-      {loading && (
-        <ActivityIndicator color={typeConfig?.activityIndicatorColor} />
-      )}
-      {showText && (
-        <Text
-          type={sizeConfig?.textType}
-          className={clsx('text-center', typeConfig.text)}
-        >
-          {children}
-        </Text>
+      {loading && <ActivityIndicator color={typeConfig?.iconColor} />}
+      {showContent && (
+        <>
+          {Icon}
+          {!!children && (
+            <Text
+              type={sizeConfig?.textType}
+              className={clsx('text-center px-1', typeConfig.text)}
+            >
+              {children}
+            </Text>
+          )}
+        </>
       )}
     </Pressable>
   );
