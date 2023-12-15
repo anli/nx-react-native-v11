8a2bf7c4a4e965ba3ebc76e7221c5257e0e60398
Su Anli <anli@users.noreply.github.com>
2023-12-15 13:28:47 +0800

    feat(shared-ui): add FilterChip (#18)

---
+++ libs/shared/ui/src/chip/filter-chip/filter-chip.tsx
@@ -0,0 +1,82 @@
+import { FC, PropsWithChildren } from 'react';
+import { Text } from '../../text';
+import clsx from 'clsx';
+import { Pressable } from '../../pressable';
+import { View } from '../../view';
+import {
+  PlusIcon as PlusIconNative,
+  CheckIcon as CheckIconNative,
+} from 'react-native-heroicons/solid';
+import { styled } from 'nativewind';
+import { FilterChipProvider, useFilterChip } from './filter-chip-context';
+
+const PlusIcon = styled(PlusIconNative, { classProps: ['fill', 'stroke'] });
+
+const CheckIcon = styled(CheckIconNative, { classProps: ['fill', 'stroke'] });
+
+type FilterChipProps = {
+  fixed: boolean;
+  onValueChange?: (value?: string) => void;
+  defaultValue?: string;
+};
+
+const FilterChipComponent: FC<PropsWithChildren<FilterChipProps>> = ({
+  fixed = false,
+  onValueChange,
+  defaultValue,
+  children,
+}) => {
+  return (
+    <FilterChipProvider
+      defaultValue={defaultValue}
+      fixed={fixed}
+      onValueChange={onValueChange}
+    >
+      <View className={clsx('flex-row')} style={{ gap: 12 }}>
+        {children}
+      </View>
+    </FilterChipProvider>
+  );
+};
+
+type FilterChipOptionProps = {
+  title: string;
+  id: string;
+};
+
+const FilterChipOption: FC<FilterChipOptionProps> = ({ id, title }) => {
+  const { value, setValue, fixed } = useFilterChip();
+  const selected = value === id;
+
+  const handlePress = () => setValue((_id) => (id === _id ? undefined : id));
+
+  return (
+    <Pressable
+      className={clsx(
+        'flex-row bg-white rounded-md active:opacity-50 items-center justify-between px-3 border-solid border',
+        selected ? 'border-green-500' : 'border-transparent',
+        fixed ? 'flex-1' : ''
+      )}
+      style={{ gap: 8 }}
+      onPress={handlePress}
+    >
+      <Text
+        type="body1"
+        className={clsx('py-3', 'text-black')}
+        numberOfLines={1}
+      >
+        {title}
+      </Text>
+      {selected && (
+        <CheckIcon fill="fill-green-500" stroke="stroke-green-500" size={20} />
+      )}
+      {!selected && (
+        <PlusIcon fill="fill-gray-400" stroke="stroke-gray-400" size={20} />
+      )}
+    </Pressable>
+  );
+};
+
+export const FilterChip = Object.assign(FilterChipComponent, {
+  Option: FilterChipOption,
+});
